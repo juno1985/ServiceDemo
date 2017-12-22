1f9521e12ff863a2b7983debe8b77942d89c8100
Jun <wangzhen_tju@126.com>
2017-12-23 07:04:25 +0800

    使用deps参数进行工厂参数注入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,8 +22,8 @@ import { Product2Component } from './product2/product2.component';
   //声明service,为将来的注入
   providers: [{
     provide:ProductService,
-    useFactory:()=>{
-      let logger = new LoggerService();
+    useFactory:(logger:LoggerService)=>{
+      
       //大于0.5返回true,否则false
       let dev=Math.random()>0.5;
       if(dev){
@@ -31,7 +31,9 @@ import { Product2Component } from './product2/product2.component';
       }else{
         return new AnotherProductService(logger);
       }
-    }
+    },
+    //使用deps参数进行工厂参数注入
+    deps:[LoggerService]
   }, 
     LoggerService],
   bootstrap: [AppComponent]
