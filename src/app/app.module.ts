c6d80e97b06a9a6b228004ded58739c5052bc9b9
Jun <wangzhen_tju@126.com>
2017-12-23 07:01:42 +0800

    使用工厂方法动态创建service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AnotherProductService } from './share/another-product.service';
 import { LoggerService } from './share/logger.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -19,7 +20,20 @@ import { Product2Component } from './product2/product2.component';
     BrowserModule
   ],
   //声明service,为将来的注入
-  providers: [ProductService, LoggerService],
+  providers: [{
+    provide:ProductService,
+    useFactory:()=>{
+      let logger = new LoggerService();
+      //大于0.5返回true,否则false
+      let dev=Math.random()>0.5;
+      if(dev){
+        return new ProductService(logger);
+      }else{
+        return new AnotherProductService(logger);
+      }
+    }
+  }, 
+    LoggerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
